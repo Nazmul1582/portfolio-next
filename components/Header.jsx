6e12ace703017b0bef63e768b4547ad51bc6d9aa
Nazmul1582 <nazmulhasan1582@gmail.com>
2026-07-28 16:49:31 +0600

    add motion effect in mobile menu button and header's nav links

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,6 +2,7 @@
 import { Menu } from "lucide-react";
 import Link from "next/link";
 import { useState } from "react";
+import { motion, AnimatePresence } from "motion/react";
 
 export default function Header() {
   const [open, setOpen] = useState(false);
@@ -53,47 +54,64 @@ export default function Header() {
                   href={`#${item.href}`}
                   className="text-sm font-medium text-slate-400 hover:text-white transition-colors"
                 >
-                  {item.title}
+                  <motion.button
+                    whileTap={{ scale: 1.1 }}
+                    className="cursor-pointer"
+                  >
+                    {item.title}
+                  </motion.button>
                 </Link>
               ))}
 
-              <Link
-                href="#contact"
-                className="px-5 py-2.5 text-sm font-medium bg-gradient text-white rounded-full transition-all hover:scale-105"
-              >
-                Contact
-              </Link>
+              <motion.button whileTap={{ scale: 1.1 }}>
+                <Link
+                  href="#contact"
+                  className="px-5 py-2.5 text-sm font-medium bg-gradient text-white rounded-full transition-all hover:scale-105"
+                >
+                  Contact
+                </Link>
+              </motion.button>
             </div>
-            <button
+            <motion.button
+              whileTap={{ scale: 1.1 }}
               onClick={() => setOpen((prev) => !prev)}
               className="md:hidden p-2 text-slate-400 hover:text-white"
             >
               <Menu className="size-5" />
-            </button>
+            </motion.button>
           </div>
         </div>
-        <div
-          className={`${open ? "block" : "hidden"} md:hidden glass border-t border-slate-800`}
-        >
-          <div className="container mx-auto pl-2 pr-4 pt-2 pb-6 space-y-2">
-            {navLinks.map((item) => (
-              <Link
-                key={item.id}
-                href={`#${item.href}`}
-                className="block pl-2 py-2 text-base font-medium text-slate-300 hover:text-white hover:bg-slate-800 rounded-lg"
-              >
-                {item.title}
-              </Link>
-            ))}
 
-            <Link
-              href="#contact"
-              className="block pl-2 py-2 text-base font-medium text-gradient hover:opacity-80"
+        <AnimatePresence>
+          {open && (
+            <motion.div
+              initial={{ opacity: 0, y: -10 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -10 }}
+              key="mobile-menu"
+              className="md:hidden glass border-t border-slate-800"
             >
-              Contact Me →
-            </Link>
-          </div>
-        </div>
+              <div className="container mx-auto pl-2 pr-4 pt-2 pb-6 space-y-2">
+                {navLinks.map((item) => (
+                  <Link
+                    key={item.id}
+                    href={`#${item.href}`}
+                    className="block pl-2 py-2 text-base font-medium text-slate-300 hover:text-white hover:bg-slate-800 rounded-lg"
+                  >
+                    {item.title}
+                  </Link>
+                ))}
+
+                <Link
+                  href="#contact"
+                  className="block pl-2 py-2 text-base font-medium text-gradient hover:opacity-80"
+                >
+                  Contact Me →
+                </Link>
+              </div>
+            </motion.div>
+          )}
+        </AnimatePresence>
       </nav>
     </header>
   );
